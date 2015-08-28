9d8106a17ba608f1fe34b54c029949e07385c1e0
Brook Elgie <specialbrew@gmail.com>
2015-08-28 12:31:23 +0100

    A fluent dataset title field.
    This introduces a `fluent_title` multilanguage text field.
    pkg_dict['title'] is populated in `after_show` and `before_view` by
    getting the value for the current language from `fluent_title`.

--- ckanext/panama/plugin.py
+++ ckanext/panama/plugin.py
@@ -1,14 +1,17 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.lib.helpers as lib_helpers
 
-# from ckanext.panama.validators import panama_fluent_text_output
 from ckanext.panama.helpers import get_default_locale
+
+import logging
+log = logging.getLogger(__name__)
 
 
 class PanamaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    # plugins.implements(plugins.IValidators)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -17,9 +20,31 @@ class PanamaPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'panama')
 
-    # # IValidators
-    # def get_validators(self):
-    #     return {'panama_text_output': panama_fluent_text_output}
+    # ITemplateHelpers
 
     def get_helpers(self):
         return {'get_default_locale': get_default_locale}
+
+    # IPackageController
+
+    def _add_to_pkg_dict(self, pkg_dict):
+        '''Ensure pkg_dict['title'] is set to the current lang if available,
+        or default locale if not.'''
+        fluent_title = pkg_dict.get('fluent_title')
+        if fluent_title:
+            current_lang = lib_helpers.lang()
+            if fluent_title.get(current_lang):
+                pkg_dict['title'] = fluent_title[current_lang]
+            else:
+                pkg_dict['title'] = \
+                    fluent_title[get_default_locale()]
+
+        return pkg_dict
+
+    def before_view(self, pkg_dict):
+
+        return self._add_to_pkg_dict(pkg_dict)
+
+    def after_show(self, context, pkg_dict):
+
+        return self._add_to_pkg_dict(pkg_dict)
