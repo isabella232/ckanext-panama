e68d5644aae36c34f7856f26adef2f22ea3a9c81
dumyan <damjan.velickovski@keitaro.info>
2016-06-15 09:06:14 +0000

    If canvas not supported by browser, fallback to old tagcloud

--- ckanext/panama/fanstatic/js/tagmodule.js
+++ ckanext/panama/fanstatic/js/tagmodule.js
@@ -1,6 +1,128 @@
 'use strict';
 
 ckan.module('tagmodule', function ($, _) {
+
+  function fallbackTagCloud () {
+    /*!
+     * jquery.tagcloud.js
+     * A Simple Tag Cloud Plugin for JQuery
+     *
+     * https://github.com/addywaddy/jquery.tagcloud.js
+     * created by Adam Groves
+     */
+    (function ($) {
+
+      /*global jQuery*/
+      "use strict";
+
+      var compareWeights = function (a, b) {
+        return a - b;
+      };
+
+      // Converts hex to an RGB array
+      var toRGB = function (code) {
+        if (code.length === 4) {
+          code = code.replace(/(\w)(\w)(\w)/gi, "\$1\$1\$2\$2\$3\$3");
+        }
+        var hex = /(\w{2})(\w{2})(\w{2})/.exec(code);
+        return [parseInt(hex[1], 16), parseInt(hex[2], 16), parseInt(hex[3], 16)];
+      };
+
+      // Converts an RGB array to hex
+      var toHex = function (ary) {
+        return "#" + jQuery.map(ary, function (i) {
+            var hex = i.toString(16);
+            hex = (hex.length === 1) ? "0" + hex : hex;
+            return hex;
+          }).join("");
+      };
+
+      var colorIncrement = function (color, range) {
+        return jQuery.map(toRGB(color.end), function (n, i) {
+          return (n - toRGB(color.start)[i]) / range;
+        });
+      };
+
+      var tagColor = function (color, increment, weighting) {
+        var rgb = jQuery.map(toRGB(color.start), function (n, i) {
+          var ref = Math.round(n + (increment[i] * weighting));
+          if (ref > 255) {
+            ref = 255;
+          } else {
+            if (ref < 0) {
+              ref = 0;
+            }
+          }
+          return ref;
+        });
+        return toHex(rgb);
+      };
+
+      $.fn.tagcloud = function (options) {
+
+        var opts = $.extend({}, $.fn.tagcloud.defaults, options);
+        var tagWeights = this.map(function () {
+          return $(this).attr("rel");
+        });
+        tagWeights = jQuery.makeArray(tagWeights).sort(compareWeights);
+        var lowest = tagWeights[0];
+        var highest = tagWeights.pop();
+        var range = highest - lowest;
+        if (range === 0) {
+          range = 1;
+        }
+        // Sizes
+        var fontIncr, colorIncr;
+        if (opts.size) {
+          fontIncr = (opts.size.end - opts.size.start) / range;
+        }
+        // Colors
+        if (opts.color) {
+          colorIncr = colorIncrement(opts.color, range);
+        }
+        return this.each(function () {
+          var weighting = $(this).attr("rel") - lowest;
+          if (opts.size) {
+            $(this).css({"font-size": opts.size.start + (weighting * fontIncr) + opts.size.unit});
+          }
+          if (opts.color) {
+            $(this).css({"color": tagColor(opts.color, colorIncr, weighting)});
+          }
+        });
+      };
+
+      $.fn.tagcloud.defaults = {
+        size: {start: 14, end: 18, unit: "pt"}
+      };
+
+    })(jQuery);
+
+    (function ($) {
+      $.fn.shuffle = function () {
+        var allElems = this.get(),
+          getRandom = function (max) {
+            return Math.floor(Math.random() * max);
+          },
+          shuffled = $.map(allElems, function () {
+            var random = getRandom(allElems.length),
+              randEl = $(allElems[random]).clone(true)[0];
+            allElems.splice(random, 1);
+            return randEl;
+          });
+        this.each(function (i) {
+          $(this).replaceWith($(shuffled[i]));
+        });
+        return $(shuffled);
+      };
+    })(jQuery);
+
+    $("#tags a").shuffle();
+    $("#tags a").tagcloud({
+      size: {start: 18, end: 40, unit: "px"},
+      color: {start: '#848484', end: '#1E1E1E'}
+    });
+  }
+
   return {
     initialize: function () {
       console.log(this.el.context.id);
@@ -16,8 +138,8 @@ ckan.module('tagmodule', function ($, _) {
           });
         } catch (e) {
           // something went wrong, hide the canvas container
-          console.log(e);
-          $('#myCanvasContainer').style.display = 'none';
+          $('#tagcloud').addClass('hidden');
+          fallbackTagCloud();
         }
       })();
     }
