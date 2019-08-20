b466437abe08f3a24108e0819e652c801af11469
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-08-20 10:27:08 +0400

    Uppercase formats

--- ckanext/panama/validators.py
+++ ckanext/panama/validators.py
@@ -4,4 +4,4 @@ def format_validator(key, data, errors, context):
     if not data[key]:
         return
     # Make sure similar fomrmats look same Eg .csv, csv, CSV
-    data[key] = data[key].lower().replace('.', '')
+    data[key] = data[key].upper().replace('.', '')
